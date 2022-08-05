ef07ada9a57d6ce1dcddfb1ba18bfc28f2cfc977
gavand-sagar <102892081+gavand-sagar@users.noreply.github.com>
2022-08-05 18:51:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo!')
+    res.send('Welcome to sagar-gavand.tech')
 })
 app.listen(process.env.PORT || 3000)
