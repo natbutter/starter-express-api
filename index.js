b9d593e94495e9ec25a53ad6df8412ecf545ed6b
root <root@DESKTOP-43CAOOI.localdomain>
2022-10-17 13:05:03 +1100

    new changes

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
+app.use(cors())
+app.get('/',(req,res) => {
+	res.send("hello world")
+})
+
+app.get('/get-author',(req,res) => {
+	res.send("nat!")
+}
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Welcome to sagar-gavand.tech')
 })
+
 app.listen(process.env.PORT || 3000)
